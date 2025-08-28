9b6b20cd440ad1336620d44735fc1510ae8002e6
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2025-08-28 16:17:04 +1000

    Navbar completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Navbar from "./components/Navbar";
+
 export default function Home() {
-  return <h1>Hello!</h1>;
+  return (
+    <div className="relative overflow-clip" id="home">
+      <Navbar />
+    </div>
+  );
 }
