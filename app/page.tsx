26638ea5ad519666ac45b6041cb56e65f7fca7d3
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2025-08-30 15:55:12 +1000

    added top background

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import TopBackground from "./components/Hero/TopBackground";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div className="relative overflow-clip" id="home">
+      <div className="w-screen h-screen"></div>
       <Navbar />
+      <TopBackground />
     </div>
   );
 }
