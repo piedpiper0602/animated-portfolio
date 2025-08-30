fa4fc4e3dbc01ba4e09135bff609d23174028dfb
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2025-08-30 23:26:43 +1000

    completed hero section

---
+++ app/components/Sections.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from './Hero/Hero'
+
+const Sections = () => {
+  return (
+    <main className='flex flex-col gap-[142px] w-full md:max-w-screen-lg 
+    pt-[236px] md:pt-60 mx-auto'>
+        <Hero />
+    </main>
+  )
+}
+
+export default Sections
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import TopBackground from "./components/Hero/TopBackground";
 import Navbar from "./components/Navbar";
+import Sections from "./components/Sections";
 
 export default function Home() {
   return (
     <div className="relative overflow-clip" id="home">
-      <div className="w-screen h-screen"></div>
+      <Sections />
       <Navbar />
       <TopBackground />
     </div>
