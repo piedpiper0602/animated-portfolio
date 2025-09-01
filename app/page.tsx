22c196bf6b80f0f77246f4606ed510ad93ae599e
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2025-09-01 12:16:29 +1000

    implemented themetoggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import TopBackground from "./components/Hero/TopBackground";
 import Navbar from "./components/Navbar";
 import Sections from "./components/Sections";
+import ThemeToggle from "./components/ThemeToggle";
 
 export default function Home() {
   return (
     <div className="relative overflow-clip" id="home">
       <Sections />
       <Navbar />
+      <ThemeToggle />
       <TopBackground />
     </div>
   );
