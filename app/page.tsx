cffcaafc0a6c7dc7a4082f3a94fcfe742d3b9e93
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2025-09-01 12:28:13 +1000

    added bottom gradient background

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import TopBackground from "./components/Hero/TopBackground";
 import Navbar from "./components/Navbar";
 import Sections from "./components/Sections";
@@ -10,6 +11,20 @@ export default function Home() {
       <Navbar />
       <ThemeToggle />
       <TopBackground />
+      <Image 
+        src="/bottom_gradient_mobile.svg" 
+        alt="bottom gradient"
+        height={700} width={1024}
+        className="absolute bottom-0 min-w-[1024px] min-h-[700px]
+        -z-50 md:hidden"
+      />
+      <Image 
+        src="bottom_gradient.svg" 
+        alt="bottom gradient"
+        height={936} width={1557}
+        className="absolute -bottom-[175px] left-1/2 -translate-x-1/2 
+        min-w-[1557px] min-h-[936px] -z-50 hidden md:block"
+      />
     </div>
   );
 }
