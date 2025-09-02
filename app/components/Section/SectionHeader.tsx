9a3ee12e25c65a47f0350c79e304ef6ca0d481b1
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2025-09-02 12:32:47 +1000

    added animation for skill section

--- app/components/Section/SectionHeader.tsx
+++ app/components/Section/SectionHeader.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Reveal from '../Reveal';
 
 type Props = {
     plaintext: string;
@@ -7,9 +8,11 @@ type Props = {
 
 const SectionHeader = ({plaintext, highlightText}: Props) => {
   return (
+    <Reveal initialX={-40}>
     <h2 className='text-[22px] md:text-[35px] px-20 text-center'>
         {plaintext}{" "}<span className='highlight px-2 whitespace-nowrap'>{highlightText}</span>
     </h2>
+    </Reveal>
   )
 }
 
--- app/components/Skills/Skill.tsx
+++ app/components/Skills/Skill.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useRef } from 'react'
+import { motion, useMotionTemplate, useMotionValue } from 'framer-motion';
+import Reveal from '../Reveal';
 
 type Props = {
     icon: string;
@@ -6,12 +9,44 @@ type Props = {
 }
 
 const Skill = ({icon, name}: Props) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const xDistance = useMotionValue(0);
+  const yDistance = useMotionValue(0);
+  const mask = useMotionTemplate`radial-gradient(100px 100px 
+  at ${xDistance}px ${yDistance}px, #000, transparent)`
+
+  
+  
+  const handleMouseMove = (e: MouseEvent) => {
+    if(!ref.current) return;
+
+    const clientRect = ref.current.getBoundingClientRect();
+
+    xDistance.set(e.x - clientRect.x);
+    yDistance.set(e.y - clientRect.y);
+  }
+  
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+  },[]);
+
   return (
+    <Reveal duration={1.5}>
     <div className='relative flex gap-2 p-2 border-primary border 
     rounded-lg h-[46px]'>
+      <motion.div 
+      ref={ref}
+      className='absolute inset-0 border-2
+      border-purple-500 dark:border-purple-300 
+      rounded-lg'
+      style={{
+        maskImage: mask,
+        WebkitMaskImage: mask,
+      }}></motion.div>
         <img src={icon} alt={`${name} icon`} />
         <p className='text-lg'>{name}</p>
     </div>
+    </Reveal>
   )
 }
 
